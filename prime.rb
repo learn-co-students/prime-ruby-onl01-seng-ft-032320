9d371646afe9dd01c63ba8b52a40550245233922
Trevor Tarpinian <tmtarpinian@gmail.com>
2020-03-27 16:51:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+  if number < 0 || number == 0 ||number == 1
+    return false
+  elsif number > 2
+    (2..(number-1)).each do |i|
+      return false if number % i == 0
+      end
+  end
+  true
+end
