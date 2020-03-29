a9a0dc2681f48bab77d43795f04e378c9de419a8
edmundo papi <edmundopierre@yahoo.com>
2020-03-29 05:29:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+# # Add  code here!
+ def prime?(number)
+
+return false if number <2
+return true if number==2
+(2..number-1).each{ |value| return false if number%value ==0}
+
+true
+end
