64453c04507bf0c19186b9854861e03ccab5fc8e
Nicholas Keller <nic.keller328@gmail.com>
2020-03-30 19:22:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+  square_root = number**0.5
+  round = square_root
+  range = (2..round).to_a
+  i = 1
+  tf_array = []
+  if number == 2 || number == 3
+    return true
+  else
+    range.each do |num|
+      tf_array << (number % num == 0)
+  end
+  end
+ if tf_array.any? == false
+   return true
+ else
+   return false
+ end
+end
