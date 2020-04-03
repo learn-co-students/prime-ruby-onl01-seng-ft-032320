49d975072cf26c6a5ca8c4c0ccb53ada8ee42c8b
chris brady <chrisb3546@yahoo.com>
2020-04-03 16:15:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(integer)
+  if integer == 0 || integer == 1 || integer < 0
+    return false
+  end 
+    ##if integer % number between 2 and integer == 0
+       (2..integer-1).to_a.all? do |number|
+          integer % number != 0
+           
+         
+       end
+
+      ##return false
+    
+end
